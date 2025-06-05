dd83616888f55681c0b17fcc269ead65da655d10
“AbdurRaahimm” <rahim703936@gmail.com>
2025-06-05 15:10:51 +0600

    Refactor Vite configuration for conditional base path and add local config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   base: '/learn-react-hook-form/',
+//   plugins: [react(), tailwindcss()],
+//   build: {
+//     outDir: 'build'
+//   }
+// });
+
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/learn-react-hook-form/" : "/",
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'build'
-  }
-});
+    outDir: "build",
+  },
+}));
